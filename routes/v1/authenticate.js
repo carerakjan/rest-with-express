011366d889984ee6ba2ca4eed0e53eff0644ed75
bohdanh <bohdan.hryhorian@playtech.com>
2016-03-15 12:02:08 +0200

    added handling of superuser

--- routes/v1/authenticate.js
+++ routes/v1/authenticate.js
@@ -5,12 +5,49 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var scrambler = require('../../lib/scrambler')();
 
+var generateToken = function(user, req, res, next) {
+
+    var conf = config.get('scrambler');
+    conf.secret = req.body.password;
+    conf.key = user.password;
+
+    scrambler.verify(conf).then(function(isMatch) {
+        if (!isMatch) {
+            next(new Error('Authentication failed. Wrong password.'));
+        } else {
+            // if user is found and password is right
+            // create a token
+            var token = jwt.sign(user,
+                config.get('jwt:secret') + req.headers['user-agent'],
+                config.get('jwt:options:sign'));
+
+            // return the information including token as JSON
+            res.json({token: token});
+        }
+    }, next);
+
+};
+
+var superuser = function(req, res, next) {
+    if(req.body.isRoot === true) {
+        var user = config.get('superuser');
+        if(user && req.body.login === user.login) {
+            generateToken(user, req, res, next);
+        } else {
+            next(new Error('Authentication failed. User not found.'));
+        }
+    } else {
+        next();
+    }
+};
+
 var validator = require('../../middleware/validator')({schema:{
     type: 'object',
     properties: {
         login: { type:'string' },
-        password: { type: 'string'}
-    },
+        password: { type: 'string' },
+        isRoot: { type: 'boolean' }
+     },
     required: ['login', 'password']
 }, options:{
     removeAdditional: true
@@ -19,35 +56,15 @@ var validator = require('../../middleware/validator')({schema:{
 var storage = require('../../middleware/storage')(config.get('storage'));
 
 /* API. */
-router.post('/', validator.middleware(), storage.middleware(), function(req, res, next) {
+router.post('/', validator.middleware(), superuser, storage.middleware(), function(req, res, next) {
     // find the user
     req.storage('users').findOne({
         login: req.body.login
     }).then(function(user) {
-
         if (!user) {
             next(new Error('Authentication failed. User not found.'));
         } else if (user) {
-
-            var conf = config.get('scrambler');
-            conf.secret = req.body.password;
-            conf.key = user.password;
-
-            // check if password matches
-            scrambler.verify(conf).then(function(isMatch) {
-                if (!isMatch) {
-                    next(new Error('Authentication failed. Wrong password.'));
-                } else {
-                    // if user is found and password is right
-                    // create a token
-                    var token = jwt.sign(user,
-                        config.get('jwt:secret') + req.headers['user-agent'],
-                        config.get('jwt:options:sign'));
-
-                    // return the information including token as JSON
-                    res.json({token: token});
-                }
-            }, next);
+            generateToken(user, req, res, next);
         }
     }, next);
 });
