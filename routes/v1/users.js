28d67dbcf866e5c1e2175002ad5766f8aad7169b
Bogdan Hryhorian <carerakjan@gmail.com>
2016-03-20 15:11:23 +0200

    added put handler

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -2,7 +2,6 @@ var path = require('path');
 var config = require('config');
 var express = require('express');
 var router = express.Router();
-var jwt = require('jsonwebtoken');
 var scrambler = require(config.get('lib:scrambler'))();
 var dbName = 'users';
 
@@ -19,36 +18,11 @@ var validator = require(config.get('middleware:validator'))({schema:{
 
 var storage = require(config.get('middleware:storage'))(config.get('storage'));
 
-var security = function(req, res, next) {
-
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    // decode token
-    if (token) {
-
-        // verifies secret and checks exp
-        jwt.verify(token, config.get('jwt:secret') + req.headers['user-agent'], function (err, decoded) {
-            if (err) {
-                next(new Error('Failed to authenticate token.'));
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-
-    } else {
-        // if there is no token
-        // return an error
-        var error = new Error('No token provided.');
-        error.status = 403;
-        next(error);
-    }
-};
+var security = require(config.get('middleware:security'));
 
     /* Middlewares. */
 router.use(storage.middleware());
+router.use(security.middleware());
 
 /* API. */
 router.get('/count', function(req, res, next) {
@@ -57,25 +31,31 @@ router.get('/count', function(req, res, next) {
     }, next);
 });
 
-router.get('/', security, function(req, res, next) {
+router.get('/', function(req, res, next) {
     req.storage(dbName).find({}).then(function(docs) {
         res.json({data: docs});
     }, next);
 });
 
-router.get('/:id', security, function(req, res, next) {
+router.get('/:id', function(req, res, next) {
     req.storage(dbName).findOne({_id: req.params.id}).then(function(docs) {
         res.json({data: docs});
     }, next);
 });
 
-router.delete('/:id', security, function(req, res, next) {
+router.delete('/:id', function(req, res, next) {
     req.storage(dbName).remove({_id: req.params.id}).then(function(docs) {
         res.json({data: docs});
     }, next);
 });
 
-router.post('/', security, validator.middleware(), function(req, res, next) {
+router.put('/:id', validator.middleware(), function(req, res, next) {
+    req.storage(dbName).update({_id: req.params.id}, {$set: req.body}).then(function(docs) {
+        res.json({data: docs});
+    }, next);
+});
+
+router.post('/', validator.middleware(), function(req, res, next) {
 
     var conf = config.get('scrambler');
     conf.secret = req.body.password;
